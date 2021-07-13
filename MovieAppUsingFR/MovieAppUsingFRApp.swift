add22b0e3e0d11edbeefb30c3a31b4be38df2548
Greg <greg@passivelogic.com>
2021-07-13 13:31:00 -0600

    Initial Commit

---
+++ MovieAppUsingFR/MovieAppUsingFRApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppUsingFRApp.swift
+//  MovieAppUsingFR
+//
+//  Created by Greg Hughes on 7/13/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppUsingFRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
